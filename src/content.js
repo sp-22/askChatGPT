1926bcf099ca979e17d4639bd1984d5aa2656e7c
sp-22 <shubhampancholi2000@gmail.com>
2023-01-03 17:14:46 +0530

    all fun done

--- src/content.js
+++ src/content.js
@@ -25,7 +25,54 @@ function getAnswer(question) {
     container.classList.add("sidebar-free");
     document.getElementById("rcnt").appendChild(container);
   }
+  connectBackground(question,container);
+  //conecting background script to get info
+  // const port = chrome.runtime.connect();
+  // port.onMessage.addListener(function (msg) {
+  //   if (msg.answer) {
+  //     container.innerHTML = `<p>${msg.answer}</p>`;
+  //   } else if (msg.error === "UNAUTHORIZED") {
+  //     container.innerHTML =
+  //       '<p>Please login at <a href="https://chat.openai.com" target="_blank">chat.openai.com</a> first</p>';
+  //   } else {
+  //     container.innerHTML = "<p>Failed to load response from ChatGPT</p>";
+  //   }
+  // });
+  // port.postMessage({
+  //   action: "callGPT",
+  //   question: question,
+  // });
+}
 
+//to open model on event of context menu
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type === "OPEN_MODAL") {
+    const selectedText = request.selectedText;
+    console.log(selectedText);
+    const modalx = `<dialog><div id="myModal" class="modal">
+    <div class="modal-content">
+      <div class="modal-title-bar">
+        <div class="logo">
+          <img src="logo.png" alt="Logo">
+          <span class="name">Ask ChatGPT</span>
+        </div>
+        <button class="close-button" onclick="closeModal()">&times;</button>
+      </div>
+      <div class="modal-body" id= "modalx-body"></div>
+    </div>
+  </div></dialog>`
+  document.body.innerHTML += modalx;
+  var dialog = document.querySelector("dialog")
+  dialog.querySelector("button").addEventListener("click", function() {
+    dialog.close()
+  })
+  dialog.showModal()
+  const container = document.getElementById("modalx-body");
+  connectBackground(selectedText,container);  
+}
+});
+
+async function connectBackground(question,container){
   //conecting background script to get info
   const port = chrome.runtime.connect();
   port.onMessage.addListener(function (msg) {
@@ -43,11 +90,3 @@ function getAnswer(question) {
     question: question,
   });
 }
-
-//to open model from context menu
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === "OPEN_MODAL") {
-    const selectedText = request.selectedText;
-    console.log(selectedText);
-  }
-});
