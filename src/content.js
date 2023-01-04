816279c57cf3cfca568c0a9a84a585d900a9a936
sp-22 <shubhampancholi2000@gmail.com>
2023-01-04 16:23:12 +0530

    style update

--- src/content.js
+++ src/content.js
@@ -12,12 +12,13 @@ function getAnswer(question) {
   container.className = "chat_gpt_container";
 
   const attribution = document.createElement("a");
-  attribution.textContent = "ChatGPT";
+  attribution.textContent = "Ask ChatGPT";
   attribution.href = "https://openai.com/blog/chatgpt/";
   attribution.target = "_blank";
   attribution.classList.add("chat_gpt_container_attribution");
   container.appendChild(attribution);
-
+  const newContainer = document.createElement("div");
+  container.appendChild(newContainer);
   const siderbarContainer = document.getElementById("rhs");
   if (siderbarContainer) {
     siderbarContainer.prepend(container);
@@ -25,8 +26,8 @@ function getAnswer(question) {
     container.classList.add("sidebar-free");
     document.getElementById("rcnt").appendChild(container);
   }
-  container.innerHTML = "<p>Waiting for ChatGPT ...</p>";
-  connectBackground(question,container);
+  newContainer.innerHTML = "<p>Waiting for ChatGPT ...</p>";
+  connectBackground(question,newContainer);
 }
 
 //to open model on event of context menu
@@ -34,20 +35,22 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === "OPEN_MODAL") {
     const selectedText = request.selectedText;
     console.log(selectedText);
-    const modalx = `<dialog><div id="myModal" class="modal">
-    <div class="modal-content">
+    const modalx = `<dialog class="main-dialog">
       <div class="modal-title-bar">
-        <div class="logo">
-          <img src="logow.png" alt="Logo">
-          <span class="name">Ask ChatGPT</span>
-        </div>
-        <button class="close-button" onclick="closeModal()">&times;</button>
+        <span class="name">Ask ChatGPT</span>
+        <button class="close-button">&times;</button>
       </div>
-      <div class="modal-body" id= "modalx-body"></div>
-    </div>
-  </div></dialog>`
+      <div class="modal-content">
+        <span class="modal-content-title">Prompt</span>
+        <span id= "modal-prompt"></span>
+        <span class="modal-content-title">Response</span>
+        <div class="modal-body" id= "modalx-body"></div>
+      </div>
+      <span class="modal-footer">Response generated with ChatGPT by OpenAI<span>
+    </dialog>`
   document.body.innerHTML += modalx;
   var dialog = document.querySelector("dialog")
+  document.getElementById("modal-prompt").innerHTML = selectedText;
   dialog.querySelector("button").addEventListener("click", function() {
     dialog.close()
   })
@@ -62,12 +65,12 @@ async function connectBackground(question,container){
   const port = chrome.runtime.connect();
   port.onMessage.addListener(function (msg) {
     if (msg.answer) {
-      container.innerHTML = `<p>${msg.answer}</p>`;
+      container.innerHTML = `${msg.answer}`;
     } else if (msg.error === "UNAUTHORIZED") {
       container.innerHTML =
-        '<p>Please login at <a href="https://chat.openai.com" target="_blank">chat.openai.com</a> first</p>';
+        'Please login at <a href="https://chat.openai.com" target="_blank">chat.openai.com</a> first';
     } else {
-      container.innerHTML = "<p>Failed to load response from ChatGPT</p>";
+      container.innerHTML = "Failed to load response from ChatGPT";
     }
   });
   port.postMessage({
